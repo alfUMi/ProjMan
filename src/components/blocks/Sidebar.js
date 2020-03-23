38da182145e21326cfd49176b7a196ae6b03e2a7
Denis Ostrovsky <denisostrovskis1@gmail.com>
2020-03-23 16:27:30 +0200

    added siderbar item component and styles for sidebar

--- src/components/blocks/Sidebar.js
+++ src/components/blocks/Sidebar.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
+import SidebarItem from './SidebarItem';
+
 class Sidebar extends Component {
     render() {
         return (
-            <div className="sidebar">
-                <ul className="sidebar__item"><a href="/">Dashboard</a></ul>
-                <ul className="sidebar__item">Analytics</ul>
-                <ul className="sidebar__item">Projects</ul>
-                <ul className="sidebar__item">Tasks</ul>
-                <ul className="sidebar__item">Tracking</ul>
-                <ul className="sidebar__item">History</ul>
-                <ul className="sidebar__item">Inbox</ul>
-                <ul className="sidebar__item">Settings</ul>
+            <div className="wrapper">
+                <div className="sidebar">
+                    <ul>
+                        <SidebarItem title="Dashboard"/>
+                        <SidebarItem title="Analytics"/>
+                        <SidebarItem title="Projects"/>
+                        <SidebarItem title="Tasks"/>
+                        <SidebarItem title="Tracking"/>
+                        <SidebarItem title="History"/>
+                        <SidebarItem title="Inbox"/>
+                        <SidebarItem title="Settings"/>
+                    </ul>
+                </div>
             </div>
         )
     }
--- src/components/blocks/SidebarItem.js
+++ src/components/blocks/SidebarItem.js
@@ -1,2 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class SidebarItem extends Component {
+    render () {
+        return (
+            <li className="item__block">
+                <a href="#" className="sidebar__item">{this.props.title}</a>
+            </li>
+        )
+    }
+}
+
+export default SidebarItem;
